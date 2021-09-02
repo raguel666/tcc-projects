2ed8b56277047cf4cd457464ec6224a9a7da5301
raguel666 <aaronjacobrobinson@icloud.com>
2021-09-02 12:05:53 +0100

    Code added for tabs, now that CSS file has been edited all seems to be in working order

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,14 @@ const navMenu = document.getElementById('nav-menu'),
 if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
-    })
+    });
 }
 
 /* =========== Hide Menu =========== */
 if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
-    })
+    });
 }
 
 /* =========== Remove Mobile Menu When Links Are Clicked =========== */
@@ -35,6 +35,7 @@ function toggleSkills() {
     for(i = 0; i < skillsContent.length; i++) {
         skillsContent[i].className = 'skills__content skills__close'
     }
+
     if (itemClass === 'skills__content skills__close') {
         this.parentNode.className = 'skills__content skills__open'
     }
@@ -42,8 +43,36 @@ function toggleSkills() {
 
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills);
-})
+});
 
 /* =========== Qualification Tabs =========== */
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /* =========== Services Modal =========== */
+
+/* =========== Portfolio Swiper =========== */
+
+/* =========== Testimonials =========== */
+
+/* =========== Scroll Sections Active Link =========== */
+
+/* =========== Change Background Color =========== */
+
+/* =========== Scroll To Top =========== */
