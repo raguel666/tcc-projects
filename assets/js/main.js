13da29e8c02fb913dfce811b5573abf0595a7499
raguel666 <aaronjacobrobinson@icloud.com>
2021-09-17 12:01:06 +0100

    scroll header added to page, styles adjusted and scroll to top added to page. Active links code added but not functioning correctly as of this moment

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,7 +125,37 @@ let swiperTestimonials = new Swiper('.testimonial__container', {
   });
 
 /* =========== Scroll Sections Active Link =========== */
+// const sections = document.querySelectorAll('section[id]')
 
-/* =========== Change Background Color =========== */
+// function scrollActive(){
+//     const scrollY = window.pageYOffset
 
-/* =========== Scroll To Top =========== */
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight
+//         const sectionTop = current.offsetTop - 50;
+//         sectionId = current.getAttribute('id')
+
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+//         }else{
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+//         }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
+
+/* =========== Add Scroll Header (Adds class which adds shadow) =========== */
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/* =========== Show Scroll To Top =========== */
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
